88c048f36327514dff3444eca1138b639a052277
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-04-30 15:46:37 +0300

    Итоговые домашние задания готовы к проверке

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        char sign = '/';
+        double result = 1;
+        if(sign == '+') {
+            result = a + b;
+        } else if(sign == '-') {
+            result = a - b;
+        } else if(sign == '*') {
+            result = a * b;
+        } else if(sign == '/') {
+            result = (double) a / b;
+        } else if(sign == '^') {
+            for(int i = 0; i < b; i++) {
+                result *= a; 
+            }
+        } else if(sign == '%') {
+            result = a % b;
+        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+}
---
+++ Lesson 1/MyFirstGame.java
@@ -0,0 +1,21 @@
+public class MyFirstGame {
+
+    public static void main(String[] args) {
+        int compNum = 23;
+        int userNum = 51;
+        int minNum = 1;
+        int maxNum = 100;
+        while(compNum != userNum) {
+            if(compNum > userNum) {
+                System.out.println("число " + userNum +" меньше того, что загадал компьютер");
+                minNum = userNum;
+                userNum = (minNum + maxNum) / 2;
+            } else {
+                System.out.println("число " + userNum +" больше того, что загадал компьютер");
+                maxNum = userNum;
+                userNum = (minNum + maxNum) / 2;
+            }
+        }
+        System.out.println("Вы победили!");
+    }
+}
