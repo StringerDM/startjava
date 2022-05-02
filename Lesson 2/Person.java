7244cfea6dcb814573d130337444b778805de94c
StringerDM <103386227+StringerDM@users.noreply.github.com>
2022-05-02 12:19:39 +0300

    1е дз готово к проверке

---
+++ Lesson 2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String sex = "mail";
+    String name = "Dmitry";
+    int height = 175;
+    int weight = 105;
+    int age = 35;
+
+    void walk() {
+        System.out.println("Человек может идти");
+    }
+
+    void sit() {
+        System.out.println("Человек может сесть");
+    }
+
+    void run() {
+        System.out.println("Человек может бежать");
+    }
+
+    void speak() {
+        System.out.println("Человек может говорить");
+    }
+
+    void learnJava() {
+        System.out.println("Человек может учить Java каждый день");
+    }
+
+}
